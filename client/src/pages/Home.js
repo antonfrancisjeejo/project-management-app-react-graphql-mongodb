1d334d886aea93c67a5f4bbb24355c6ccc3ed226
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2022-06-25 23:01:01 +0530

    Initial commit

---
+++ client/src/pages/Home.js
@@ -0,0 +1,20 @@
+import Clients from "../components/Clients";
+import Projects from "../components/Projects";
+import AddClientModal from "../components/AddClientModal";
+import AddProjectModal from "../components/AddProjectModal";
+
+const Home = () => {
+  return (
+    <>
+      <div className="d-flex gap-3 mb-4">
+        <AddClientModal />
+        <AddProjectModal />
+      </div>
+      <Projects />
+      <hr />
+      <Clients />
+    </>
+  );
+};
+
+export default Home;
