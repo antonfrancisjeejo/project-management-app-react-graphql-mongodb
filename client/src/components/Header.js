bb244efb0dfaf77390a46781cba22f163b18e518
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2022-06-26 19:45:45 +0530

    Initial commit

---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import logo from "../assets/logo.png";
+
+const Header = () => {
+  return (
+    <nav className="navbar bg-light mb-4 p-0">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          <div className="d-flex">
+            <img src={logo} alt="logo" className="mr-2" />
+            <div>ProjectMgmt</div>
+          </div>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
