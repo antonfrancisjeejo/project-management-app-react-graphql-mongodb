e5eed7f397784acf37e2c79ad138fa89d2e7ab7a
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2022-06-26 19:45:17 +0530

    Initial commit

---
+++ client/src/components/ClientRow.js
@@ -0,0 +1,37 @@
+import { FaTrash } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+import { DELETE_CLIENT } from "../graphql/mutations/clients";
+import { GET_CLIENTS } from "../graphql/queries/clients";
+import { GET_PROJECTS } from "../graphql/queries/projects";
+
+const ClientRow = ({ client }) => {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
+    //add the cache configs in App.js graphql client
+    // update(cache, { data: { deleteClient } }) {
+    //   const { clients } = cache.readQuery({ query: GET_CLIENTS });
+    //   cache.writeQuery({
+    //     query: GET_CLIENTS,
+    //     data: {
+    //       clients: clients.filter((client) => client.id !== deleteClient.id),
+    //     },
+    //   });
+    // },
+  });
+
+  return (
+    <tr>
+      <td>{client.name}</td>
+      <td>{client.email}</td>
+      <td>{client.phone}</td>
+      <td>
+        <button className="btn btn-danger btn-sm" onClick={deleteClient}>
+          <FaTrash />
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default ClientRow;
